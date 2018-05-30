ce178e5e47197b4031920eafc13fd8d49b51beda
John A. Fedoruk <johnny@johnfedoruk.ca>
2018-05-29 20:38:52 -0500

    Main file defined

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include "main.hpp"
+#include "lib/wasteful.hpp"
 
 int main(int argc, char* argv[]) {
     std::cout << "How many GB would you like to make?" << std::endl;
@@ -9,9 +11,18 @@ int main(int argc, char* argv[]) {
     std::stringstream size_stream(size_string);
     unsigned int size_int = 0;
     size_stream >> size_int;
-    if( size_int < 1 || size_int > 4 ) {
-        std::cout << "Please specify an integer between 1 and 4" << std::endl;
+    if( size_int < MIN_GB_WASTE || size_int > MAX_GB_WASTE ) {
+        std::cout << "Please specify an integer between "  << MIN_GB_WASTE << " and " << MAX_GB_WASTE << std::endl;
         return 1;
     }
+    Wasteful* waste = new Wasteful(size_int);
+    waste->makeWaste();
+    pause();
     return 0;
+}
+
+void pause(void) {
+    std::cout << '\n' << "Press a key to continue..." << std::endl;
+    std::cin.ignore();
+    std::cin.get();
 }
